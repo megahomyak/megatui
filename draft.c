027e426d3d8af0873e30fdcc055aa5e3a3615375
megahomyak <g.megahomyak@gmail.com>
2025-02-08 17:51:43 +0500

    Trying to write cool code, but it's notvery portable

--- draft.c
+++ draft.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
-#define termseq(name, value) int name(void) { return printf("%s", "\x1b[" value); }
+#define termseq(name, value) inline void name(void) { printf("%s", "\x1b[" value); }
 
 termseq(alt_screen, "?1049h")
 termseq(main_screen, "?1049l")
+termseq(clear_screen, "2J")
+
+inline void move_cursor(int x, int y) {
+    printf("\x1b[%d;%dH", x, y);
+}
 
 int main(void) {
     alt_screen();
