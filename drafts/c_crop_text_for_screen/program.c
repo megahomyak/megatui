c7b9fde7d60384b56dd7f420aafdc59fa8cfcd55
megahomyak <g.megahomyak@gmail.com>
2025-02-14 05:42:35 +0500

    Writing down some very important thoughts

--- drafts/c_crop_text_for_screen/program.c
+++ drafts/c_crop_text_for_screen/program.c
@@ -7,14 +7,43 @@ typedef struct Char {
     struct Char* next;
 } Char;
 
-void display(Char* cursor, int width, int height) {
-    int line_cursor_index = 0;
-    {
-        Char* current = cursor;
-        while ((current = current->next) != NULL) {
-            line_cursor_index++;
-        }
+typedef unsigned int uint;
+
+/* cursor should be non-null when passed in */
+void display(Char* cursor, uint width, uint height) {
+    if (width == 0 || height == 0) return;
+    /*
+    go back the same line, counting characters traveled
+    calculate on what character in its line the cursor is
+    get the amt of lines after
+    get how many lines should go before
+    */
+    // go back the same line, counting characters traveled
+    uint line_cursor_index = 0;
+    Char* cursor_line = cursor;
+    while (cursor_line->prev != NULL && cursor_line->prev->value != '\n') {
+        cursor_line = cursor_line->prev;
+        ++line_cursor_index;
     }
+
+    // calculate on what character in its line the cursor is
+    uint screen_line_cursor_index = line_cursor_index % width;
+    uint lines_before = line_cursor_index / width;
+
+    // get the amt of lines after
+    uint lines_after = 0;
+    do {
+        if (cursor->value == '\n') {
+            ++lines_after;
+            if (lines_before + lines_after + 1 >= height) {
+                break;
+            }
+        }
+        cursor = cursor->next;
+    } while (cursor != NULL);
+
+    // get how many lines should go before
+    // TODO: DO NOT TOUCH THE FUCKING "CURSOR_LINE"'s NAMING!!!! IT'S LIKE THAT FOR A REASON! IF BEFORE THE CURSOR LINE THERE'S NOTHING, WHAT'S THE CHARACTER GONNA BE? NULL? SO IT'S NOT "BEFORE_CURSOR_LINE"!!!!
 }
 
 void die_if_null(void* ptr) {
